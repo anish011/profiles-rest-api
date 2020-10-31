2a7a8982278fddc352cd68b12c7586a7026be701
Anish <anish.knkr@gmail.com>
2020-10-31 17:39:25 +0530

    Added HelloApiView

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+    """Test APU VIew"""
+    def get(self, request, format=None):
+        temp_list=[
+            'First in the list',
+            'Second in the list',
+            'Third in the list'
+        ]
+        return Response({'message':'Hello', 'an_apiview':temp_list})
