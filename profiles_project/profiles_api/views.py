41308fce4d25b16ce979a0c6493a9f6b1b3a0c64
Anish <anish.knkr@gmail.com>
2020-11-03 00:04:17 +0530

    Added POST, PUT, PATCH and DELETE methods to our APIView

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 # Create your views here.
 class HelloApiView(APIView):
     """Test APU VIew"""
+
+    serializer_class = serializers.HelloSerilaizer
+
     def get(self, request, format=None):
         temp_list=[
             'First in the list',
@@ -12,3 +18,22 @@ class HelloApiView(APIView):
             'Third in the list'
         ]
         return Response({'message':'Hello', 'an_apiview':temp_list})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
