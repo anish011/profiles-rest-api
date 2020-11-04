5cfe42650cb36c6609e859d0d9153533de007457
Anish <anish.knkr@gmail.com>
2020-11-05 02:09:33 +0530

    Added user profile feed API

--- profiles_project/profiles_api/permisssions.py
+++ profiles_project/profiles_api/permisssions.py
@@ -7,3 +7,11 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return request.user.id == obj.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.id == obj.user_profile.id
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -8,6 +8,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
@@ -93,3 +94,13 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
 class UserLoginApiViewSet(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    permission_classes = (permisssions.UpdateOwnStatus, IsAuthenticated,)
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
